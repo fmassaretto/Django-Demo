7722998b9eee6a16b43c6a2ae5df206f23c27bf5
Fabio Massaretto <fabio_no_more@yahoo.com.br>
2025-01-13 22:25:06 -0300

    project renamed

--- Django_Demo_Project/foodlog/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Food
-
-class FoodAdmin(admin.ModelAdmin):
-    lista = ('meal type', 'calories', 'proteins')
-admin.site.register(Food)
-
-# Register your models here.
--- Django_Demo_Project/foodlog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'foodlog'
-
-urlpatterns = [
-    path('', views.food_list, name='foodlog_list'),
-]
