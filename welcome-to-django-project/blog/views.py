9121ceb1fab05e5aa59b63f36d7f7e362d1265f3
Fabio Massaretto <fabio_no_more@yahoo.com.br>
2025-02-07 01:07:26 -0300

    add tag feature to posts blog

--- welcome-to-django-project/blog/views.py
+++ welcome-to-django-project/blog/views.py
@@ -4,17 +4,24 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.views.decorators.http import require_POST
 from django.core.mail import send_mail
+from taggit.models import Tag
 from .models import Post
 from .forms import EmailPostForm, CommentForm
 
 
 # Create your views here.
-def post_list(request):
-    posts_list = Post.published.all()
+def post_list(request, tag_slug=None):
+    post_list = Post.published.all()
+    
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
     
     # Pagination with 3 posts per page
-    paginator = Paginator(posts_list, 3)
+    paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
+
     try:
         posts = paginator.page(page_number)
     except PageNotAnInteger:
@@ -24,7 +31,8 @@ def post_list(request):
     
     return render(request,
                   'blog/post/list.html',
-                  {'posts': posts})
+                  {'posts': posts,
+                   'tag': tag})
     
     
 class PostListView(ListView):
